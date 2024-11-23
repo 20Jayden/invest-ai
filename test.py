1d01a49d8d88e9489c064256ebca45b2c562c67f
20Jayden <jhmoon0415@naver.com>
2024-11-24 03:22:57 +0900

    투자자 매매법 영상 변경 및 텍스트 수정

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
 # Get transcript from YouTube
-transcript = YouTubeTranscriptApi.get_transcript("TWINrTppUl4")
+transcript = YouTubeTranscriptApi.get_transcript("3XbtEX3jUv4", languages=['ko'])
 
 # Combine all text into a single variable
 combined_text = ' '.join(entry['text'] for entry in transcript)
